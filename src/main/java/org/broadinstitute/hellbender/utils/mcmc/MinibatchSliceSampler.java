91ee1f1ec00b5f4794bb1b938a59b79ad5900795
Samuel Lee <lee.samuel.k@gmail.com>
2018-12-23 22:38:45 -0500

    added minibatch backend and tests

---
+++ src/main/java/org/broadinstitute/hellbender/utils/mcmc/MinibatchSliceSampler.java
@@ -0,0 +1,173 @@
+package org.broadinstitute.hellbender.utils.mcmc;
+
+
+import org.apache.commons.math3.distribution.TDistribution;
+import org.apache.commons.math3.random.RandomGenerator;
+import org.broadinstitute.hellbender.exceptions.GATKException;
+import org.broadinstitute.hellbender.utils.Utils;
+import org.broadinstitute.hellbender.utils.param.ParamUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Implements slice sampling of a continuous, univariate, unnormalized probability density function (PDF),
+ * which is assumed to be unimodal.  See Neal 2003 at https://projecteuclid.org/euclid.aos/1056562461 for details.
+ * Minibatching is implemented as in http://proceedings.mlr.press/v33/dubois14.pdf and requires
+ * that the PDF, which is assumed to be a posterior function of a parameter value and the data, is specified in terms
+ * of a prior, a likelihood, and the data.
+ *
+ * @author Samuel Lee &lt;slee@broadinstitute.org&gt;
+ */
+public final class MinibatchSliceSampler<DATA> extends AbstractSliceSampler {
+    private final List<DATA> data;
+    private final Function<Double, Double> logPrior;
+    private final BiFunction<DATA, Double, Double> logLikelihood;
+    private final Integer minibatchSize;
+    private final Double approxThreshold;
+
+    private Double xSampleCache = null;
+    private Double logPriorCache = null;
+    private List<Double> logLikelihoodsCache = null;
+
+    /**
+     * Creates a new sampler for a bounded univariate random variable, given a random number generator, a list of data,
+     * a continuous, univariate, unimodal, unnormalized log probability density function
+     * (assumed to be a posterior and specified by a prior and a likelihood),
+     * hard limits on the random variable, a step width, a minibatch size, and a minibatch approximation threshold.
+     * @param rng                       random number generator
+     * @param data                      list of data
+     * @param logPrior                  log prior component of continuous, univariate, unimodal log posterior (up to additive constant)
+     * @param logLikelihood             log likelihood component of continuous, univariate, unimodal log posterior (up to additive constant)
+     * @param xMin                      minimum allowed value of the random variable
+     * @param xMax                      maximum allowed value of the random variable
+     * @param width                     step width for slice expansion
+     * @param minibatchSize             minibatch size
+     * @param approxThreshold           threshold for approximation used in {@link MinibatchSliceSampler#isGreaterThanSliceHeight};
+     *                                  approximation is exact when this threshold is zero
+     */
+    public MinibatchSliceSampler(final RandomGenerator rng,
+                                 final List<DATA> data,
+                                 final Function<Double, Double> logPrior,
+                                 final BiFunction<DATA, Double, Double> logLikelihood,
+                                 final double xMin,
+                                 final double xMax,
+                                 final double width,
+                                 final int minibatchSize,
+                                 final double approxThreshold) {
+        super(rng, xMin, xMax, width);
+        Utils.nonEmpty(data);
+        Utils.nonNull(logPrior);
+        Utils.nonNull(logLikelihood);
+        Utils.validateArg(minibatchSize > 1, "Minibatch size must be greater than 1.");
+        ParamUtils.isPositiveOrZero(approxThreshold, "Minibatch approximation threshold must be non-negative.");
+        this.data = Collections.unmodifiableList(new ArrayList<>(Utils.nonNull(data)));
+        this.logPrior = logPrior;
+        this.logLikelihood = logLikelihood;
+        this.minibatchSize = minibatchSize;
+        this.approxThreshold = approxThreshold;
+    }
+
+    /**
+     * Creates a new sampler for an unbounded univariate random variable, given a random number generator, a list of data,
+     * a continuous, univariate, unimodal, unnormalized log probability density function
+     * (assumed to be a posterior and specified by a prior and a likelihood),
+     * a step width, a minibatch size, and a minibatch approximation threshold.
+     * @param rng                       random number generator
+     * @param data                      list of data
+     * @param logPrior                  log prior component of continuous, univariate, unimodal log posterior (up to additive constant)
+     * @param logLikelihood             log likelihood component of continuous, univariate, unimodal log posterior (up to additive constant)
+     * @param width                     step width for slice expansion
+     * @param minibatchSize             minibatch size
+     * @param approxThreshold           threshold for approximation used in {@link MinibatchSliceSampler#isGreaterThanSliceHeight};
+     *                                  approximation is exact when this threshold is zero
+     */
+    public MinibatchSliceSampler(final RandomGenerator rng,
+                                 final List<DATA> data,
+                                 final Function<Double, Double> logPrior,
+                                 final BiFunction<DATA, Double, Double> logLikelihood,
+                                 final double width,
+                                 final int minibatchSize,
+                                 final double approxThreshold) {
+        this(rng, data, logPrior, logLikelihood, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, width, minibatchSize, approxThreshold);
+    }
+
+    /**
+     * Implements the OnSlice procedure from http://proceedings.mlr.press/v33/dubois14.pdf.
+     */
+    @Override
+    boolean isGreaterThanSliceHeight(final double xProposed,
+                                     final double xSample,
+                                     final double z) {
+        if (xProposed < xMin || xMax < xProposed) {
+            return false;
+        }
+
+        //we cache values calculated from xSample, since this method is called multiple times for the same value of xSample
+        //when expanding slice interval and proposing samples
+        if (xSampleCache == null || xSampleCache != xSample) {
+            xSampleCache = xSample;
+            logPriorCache = logPrior.apply(xSample);
+            logLikelihoodsCache = data.stream().map(d -> logLikelihood.apply(d, xSample)).collect(Collectors.toList());
+        }
+        if (!((xSampleCache == null && logPriorCache == null && logLikelihoodsCache == null) ||
+                (xSampleCache != null && logPriorCache != null && logLikelihoodsCache != null))) {
+            throw new GATKException.ShouldNeverReachHereException("Cache for xSample is in an invalid state.");
+        }
+
+        final int numDataPoints = data.size();
+        final List<Integer> permutedDataIndices = IntStream.range(0, numDataPoints).boxed().collect(Collectors.toList());
+        Collections.shuffle(permutedDataIndices, new Random(rng.nextInt()));
+        final int numMinibatches = Math.max(numDataPoints / minibatchSize, 1);
+
+        final double mu0 = (logPriorCache - logPrior.apply(xProposed) - z) / numDataPoints;
+        int numDataIndicesSeen = 0;
+        double logLikelihoodDifferencesMean = 0.;
+        double logLikelihoodDifferencesSquaredMean = 0.;
+
+        for (int minibatchIndex = 0; minibatchIndex < numMinibatches; minibatchIndex++) {
+            final int dataIndexStart = minibatchIndex * minibatchSize;
+            final int dataIndexEnd = Math.min((minibatchIndex + 1) * minibatchSize, numDataPoints);
+            final int actualMinibatchSize = dataIndexEnd - dataIndexStart;  //equals minibatchSize except perhaps for last minibatch
+
+            final List<Double> logLikelihoodDifferencesMinibatch = IntStream.range(dataIndexStart, dataIndexEnd).boxed()
+                    .map(permutedDataIndices::get)
+                    .map(i -> logLikelihood.apply(data.get(i), xProposed) - logLikelihoodsCache.get(i))
+                    .collect(Collectors.toList());
+            final List<Double> logLikelihoodDifferencesSquaredMinibatch = logLikelihoodDifferencesMinibatch.stream()
+                    .map(l -> Math.pow(l, 2))
+                    .collect(Collectors.toList());
+
+            final double logLikelihoodDifferencesMinibatchSum = logLikelihoodDifferencesMinibatch.stream().mapToDouble(x -> x).sum();
+            final double logLikelihoodDifferencesSquaredMinibatchSum = logLikelihoodDifferencesSquaredMinibatch.stream().mapToDouble(x -> x).sum();
+            logLikelihoodDifferencesMean =
+                    (numDataIndicesSeen * logLikelihoodDifferencesMean + logLikelihoodDifferencesMinibatchSum) /
+                            (numDataIndicesSeen + actualMinibatchSize);
+            logLikelihoodDifferencesSquaredMean =
+                    (numDataIndicesSeen * logLikelihoodDifferencesSquaredMean + logLikelihoodDifferencesSquaredMinibatchSum) /
+                            (numDataIndicesSeen + actualMinibatchSize);
+            numDataIndicesSeen += actualMinibatchSize;
+
+            if (numDataIndicesSeen == 1) {
+                break;
+            }
+
+            final double s = Math.sqrt(1. - (double) numDataIndicesSeen / numDataPoints) *
+                    Math.sqrt((logLikelihoodDifferencesSquaredMean - Math.pow(logLikelihoodDifferencesMean, 2)) / (numDataIndicesSeen - 1));
+            final double delta = 1. - new TDistribution(null, numDataIndicesSeen - 1)
+                    .cumulativeProbability(Math.abs((logLikelihoodDifferencesMean - mu0) / s));
+
+            if (delta < approxThreshold) {
+                System.out.println(String.format("%d / %d minibatches used.", minibatchIndex + 1, numMinibatches));
+                break;
+            }
+        }
+        return logLikelihoodDifferencesMean > mu0;
+    }
+}
--- src/main/java/org/broadinstitute/hellbender/utils/mcmc/SliceSampler.java
+++ src/main/java/org/broadinstitute/hellbender/utils/mcmc/SliceSampler.java
@@ -1,160 +1,77 @@
 package org.broadinstitute.hellbender.utils.mcmc;
 
 
-import org.apache.commons.math3.distribution.ExponentialDistribution;
 import org.apache.commons.math3.random.RandomGenerator;
 import org.broadinstitute.hellbender.exceptions.GATKException;
 import org.broadinstitute.hellbender.utils.Utils;
-import org.broadinstitute.hellbender.utils.param.ParamUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.OptionalInt;
 import java.util.function.Function;
 
 /**
- * Implements slice sampling of a continuous, univariate, unnormalized probability density function,
+ * Implements slice sampling of a continuous, univariate, unnormalized probability density function (PDF),
  * which is assumed to be unimodal.  See Neal 2003 at https://projecteuclid.org/euclid.aos/1056562461 for details.
  *
  * @author Samuel Lee &lt;slee@broadinstitute.org&gt;
  */
-public class SliceSampler {
-    private static final int MAXIMUM_NUMBER_OF_DOUBLINGS = 16;
-    private static final int MAXIMUM_NUMBER_OF_SLICE_SAMPLINGS = 100;
-    private static final double EPSILON = 1E-10;
-
-    private final RandomGenerator rng;
+public final class SliceSampler extends AbstractSliceSampler {
     private final Function<Double, Double> logPDF;
-    private final double xMin;
-    private final double xMax;
-    private final double width;
-    private final ExponentialDistribution exponentialDistribution;
 
     private Double xSampleCache = null;
     private Double logPDFCache = null;
 
     /**
-     * Creates a new sampler, given a random number generator, a continuous, univariate, unimodal, unnormalized
-     * log probability density function, hard limits on the random variable, and a step width.
+     * Creates a new sampler for a bounded univariate random variable, given a random number generator,
+     * a continuous, univariate, unimodal, unnormalized log probability density function,
+     * hard limits on the random variable, and a step width.
      * @param rng      random number generator
      * @param logPDF   continuous, univariate, unimodal log probability density function (up to additive constant)
      * @param xMin     minimum allowed value of the random variable
      * @param xMax     maximum allowed value of the random variable
      * @param width    step width for slice expansion
      */
-    public SliceSampler(final RandomGenerator rng, final Function<Double, Double> logPDF,
-                        final double xMin, final double xMax, final double width) {
-        Utils.nonNull(rng);
+    public SliceSampler(final RandomGenerator rng,
+                        final Function<Double, Double> logPDF,
+                        final double xMin,
+                        final double xMax,
+                        final double width) {
+        super(rng, xMin, xMax, width);
         Utils.nonNull(logPDF);
-        Utils.validateArg(xMin < xMax, "Maximum bound must be greater than minimum bound.");
-        ParamUtils.isPositive(width, "Slice-sampling width must be positive.");
-        this.rng = rng;
         this.logPDF = logPDF;
-        this.xMin = xMin;
-        this.xMax = xMax;
-        this.width = width;
-        exponentialDistribution = new ExponentialDistribution(rng, 1.);
     }
 
     /**
-     * Creates a new sampler, given a random number generator, a continuous, univariate, unimodal, unnormalized
-     * log probability density function, and a step width.
+     * Creates a new sampler for an unbounded univariate random variable, given a random number generator,
+     * a continuous, univariate, unimodal, unnormalized log probability density function,
+     * and a step width.
      * @param rng      random number generator
      * @param logPDF   continuous, univariate, unimodal log probability density function (up to additive constant)
      * @param width    step width for slice expansion
      */
-    public SliceSampler(final RandomGenerator rng, final Function<Double, Double> logPDF, final double width) {
+    public SliceSampler(final RandomGenerator rng,
+                        final Function<Double, Double> logPDF,
+                        final double width) {
         this(rng, logPDF, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, width);
     }
 
-    /**
-     * Generate a single sample from the probability density function, given an initial value to use in slice construction.
-     * @param xInitial      initial value to use in slice construction; must be in [xMin, xMax]
-     * @return              sample drawn from the probability density function
-     */
-    public double sample(final double xInitial) {
-        Utils.validateArg(xMin <= xInitial && xInitial <= xMax, "Initial point in slice sampler is not within specified range.");
-
-        //adjust xInitial if on boundary
-        final double xSample = Math.min(Math.max(xInitial, xMin + EPSILON), xMax - EPSILON);
-
-        //follow Neal 2003 procedure to slice sample with doubling of interval (assuming unimodal distribution)
-
-        //randomly position slice with given width so that it brackets xSample; position is uniformly distributed
-        double xLeft = xSample - width * rng.nextDouble();
-        double xRight = xLeft + width;
-
-        //sample the variable used to randomly pick height of slice from uniform distribution under PDF(xSample);
-        //slice height = u * PDF(xSample), where u ~ Uniform(0, 1)
-        //however, since we are working with logPDF, we instead sample z = -log u ~ Exponential(1)
-        final double z = exponentialDistribution.sample();
-
-        int k = MAXIMUM_NUMBER_OF_DOUBLINGS;
-        //expand slice interval by doubling until it brackets PDF
-        //(i.e., PDF at both ends is less than the slice height)
-        while (k > 0 && (isOverSliceHeight(xLeft, xSample, z) || isOverSliceHeight(xRight, xSample, z))) {
-            if (rng.nextBoolean()) {
-                xLeft = xLeft - (xRight - xLeft);
-            } else {
-                xRight = xRight + (xRight - xLeft);
-            }
-            k--;
-        }
-
-        //sample uniformly from slice interval until sample over slice height found, shrink slice on each iteration if not found
-        //limited to MAXIMUM_NUMBER_OF_SLICE_SAMPLINGS, after which last proposed sample is returned
-        //(shouldn't happen if width is chosen appropriately)
-        int numIterations = 1;
-        double xProposed = rng.nextDouble() * (xRight - xLeft) + xLeft;
-        while (numIterations <= MAXIMUM_NUMBER_OF_SLICE_SAMPLINGS) {
-            if (isOverSliceHeight(xProposed, xSample, z)) {
-                break;
-            }
-            if (xProposed < xSample) {
-                xLeft = xProposed;
-            } else {
-                xRight = xProposed;
-            }
-            xProposed = rng.nextDouble() * (xRight - xLeft) + xLeft;
-            numIterations++;
-        }
-        return Math.min(Math.max(xProposed, xMin + EPSILON), xMax - EPSILON);
-    }
-
-    /**
-     * Generate multiple samples from the probability density function, given an initial value to use in slice construction.
-     * @param xInitial      initial value to use in slice construction; if outside [xMin, xMax], forced to be within
-     * @param numSamples    number of samples to generate
-     * @return              samples drawn from the probability density function
-     */
-    public List<Double> sample(final double xInitial, final int numSamples) {
-        ParamUtils.isPositive(numSamples, "Number of samples must be positive.");
-        final List<Double> samples = new ArrayList<>(numSamples);
-        double xSample = xInitial;
-        for (int i = 0; i < numSamples; i++) {
-            xSample = sample(xSample);
-            samples.add(xSample);
-        }
-        return samples;
-    }
-
-    /**
-     * Returns true if PDF(xProposed) is over slice height = u * PDF(xSample), where u = exp(-z).
-     */
-    protected boolean isOverSliceHeight(final double xProposed,
-                                        final double xSample,
-                                        final double z) {
+    @Override
+    boolean isGreaterThanSliceHeight(final double xProposed,
+                                     final double xSample,
+                                     final double z) {
         if (xProposed < xMin || xMax < xProposed) {
             return false;
         }
+
+        //we cache logPDF(xSample), since this method is called multiple times for the same value of xSample
+        //when expanding slice interval and proposing samples
         if (xSampleCache == null || xSampleCache != xSample) {
-            //we cache logPDF(xSample) to avoid evaluating it multiple times
             xSampleCache = xSample;
             logPDFCache = logPDF.apply(xSample);
         }
-        if ((xSampleCache == null && logPDFCache != null) || (xSampleCache != null && logPDFCache == null)) {
-            throw new GATKException.ShouldNeverReachHereException("Cache for logPDF(xSample) is invalid.");
+        if (!((xSampleCache == null && logPDFCache == null) ||
+                (xSampleCache != null && logPDFCache != null))) {
+            throw new GATKException.ShouldNeverReachHereException("Cache for xSample is in an invalid state.");
         }
+
         return logPDF.apply(xProposed) > logPDFCache - z;
     }
 }
